bdf0f5fca3571af41c7c06851366e7ce379eb31e
Matheus Henrique <matheus.barboza.henrique@gmail.com>
2020-10-29 22:03:02 -0300

    Perfil e configuraçoes finalizada

--- edux/src/index.js
+++ edux/src/index.js
@@ -2,12 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import Login from './pages/login';
+import Configuracoes from './pages/configuracoes';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Login />
+    <Configuracoes />
   </React.StrictMode>,
   document.getElementById('root')
 );
---
+++ edux/src/pages/configuracoes/index.jsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import { Container , Form , Button } from 'react-bootstrap';
+import Menu from '../../components/menu';
+import Rodape from '../../components/rodape';
+import './index.css';
+
+const Configuracoes = () => {
+    return (
+        <div className="completo">
+        <Menu />
+        <Container className="flex">
+           <Container className='form-height'>
+                  <Container className= "Texto">
+                      <h1>EduX</h1>
+                      <h2>Atualize seu cadastro</h2>
+                      
+                  </Container>  
+  
+                <Form className='form-signin' >
+                    
+                    <br/>
+                    <h3>Atualização Cadastral</h3>
+                    <hr/>
+                    
+                    <Form.Group controlId="formBasicEma   il">
+                        <Form.Label>Email </Form.Label>
+                        <Form.Control type="email"  placeholder="Informe o email" required />
+                    </Form.Group>
+
+                  
+  
+                    <Form.Group controlId="formBasicPassword">
+                        <Form.Label>Senha</Form.Label>
+                        <Form.Control type="password"  placeholder="Senha"  required/>
+                    </Form.Group>
+                    <Button variant="primary" type="submit" >
+                        Alterar
+                    </Button>
+                    
+                    
+                </Form>
+            </Container>
+  
+              
+          </Container>         
+        <Rodape />
+        </div>
+    )
+}
+
+export default Configuracoes
